14b26ffb3bd737b61860fa7a67e137c2558e61d7
Awesome Stanley <okwaraos@funaab.edu.ng>
2022-09-09 13:59:55 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,6 +7,7 @@
  * number stored in the variable n
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
@@ -21,4 +22,5 @@ int main(void)
 	else if (n % 10 < 6 && n % 10 != 0)
 		printf("and is less than 6 and not 0\n);
 	return (0);
+
 }
