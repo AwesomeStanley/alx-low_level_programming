f69b9d3a7861c70f1060518e45f0d32a37c543ae
Awesome Stanley <okwaraos@funaab.edu.ng>
2022-09-08 18:36:28 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - prints the standard error sandwiched around the standard outpu/t
  *
- * Return: Always 0 (Success)
+ * Return: returns 1
  */
 int main(void)
 {
-	printf(""and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
 	return (1);
 }
