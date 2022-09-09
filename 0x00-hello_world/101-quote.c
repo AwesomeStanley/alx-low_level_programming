83c45388b262f4b3d79df3f0196497b9e02dc4a8
Awesome Stanley <okwaraos@funaab.edu.ng>
2022-09-08 19:19:23 -0700

    Awesome

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
+
 /**
- * main - prints the standard error sandwiched around the standard outpu/t
+ * main - Entry point
  *
- * Return: returns 1
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
