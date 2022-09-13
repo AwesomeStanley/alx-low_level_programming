d538f6a112f9e999df0012a6b8501318f6a2aadf
Awesome Stanley <okwaraos@funaab.edu.ng>
2022-09-13 03:51:21 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isaplha - this program prints out if character is a letter
+ *
+ * @c: character value
+ *
+ * Return: alpha or not
+ */
+int _isalpha(int c)
+{
+	int alpha;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		alpha = 1;
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		alpha = 1;
+	}
+	else
+	{
+		alpha = 0;
+	}
+	return (alpha);
+}
