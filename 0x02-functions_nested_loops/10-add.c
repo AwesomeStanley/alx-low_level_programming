085a4f4ce520b0b75f64ef75ba33cc2cb251c6c6
Awesome Stanley <okwaraos@funaab.edu.ng>
2022-09-13 05:07:53 -0700

    Add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - this pprogram prints every minutes
+ *
+ * @n: first value
+ * @m: second value
+ *
+ * Return: minutes
+ */
+int add(int n, int m)
+{
+	int result = n + m;
+
+	return (result);
+}
